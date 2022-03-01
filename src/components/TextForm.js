a16a4e5fc2e3b6daf9bc3c70df609ceea3ecb71e
Anjali Chauhan <56559378+anjali1102@users.noreply.github.com>
2022-03-01 17:19:55 +0530

    word length and character count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handlerUpClick = () => {
     // console.log('Uppercase Was Clicked !!' + text);
-    let newText=text.toUpperCase();
+    let newText = text.toUpperCase();
     setText(newText);
   };
 
   const handlerOnChange = (event) => {
     // console.log('On Change');
     setText(event.target.value);
-
   };
-  const [text, setText] = useState('Enter text here');
-    // setText('anjali');
+  const [text, setText] = useState("Enter text here");
+  // setText('anjali');
   return (
-    <div>
-      <h3>{props.heading}</h3>
-      <div className='mb-3'>
-        <textarea
-          className='form-control'
-          id='exampleFormControlTextarea1'
-          rows='8'
-          value={text}
-          onChange={handlerOnChange}
-        ></textarea>
+    <>
+      <div>
+        <h3>{props.heading}</h3>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="8"
+            value={text}
+            onChange={handlerOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handlerUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handlerUpClick}>
+          Convert to Lowercase
+        </button>
       </div>
-      <button className='btn btn-primary' onClick={handlerUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+
+      <div className="container">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+      </div>
+    </>
   );
 }
